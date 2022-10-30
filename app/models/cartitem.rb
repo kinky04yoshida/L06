c78ff391e3a25be5db49dd2528b0af93c9fb60b9
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-10-30 12:06:36 +0000

    dep

--- app/models/cartitem.rb
+++ app/models/cartitem.rb
@@ -1,4 +1,6 @@
 class Cartitem < ApplicationRecord
+    belongs_to :cart
+    belongs_to :product
     validates :product_id, presence: true
     validates :quantity, presence: true, 
     numericality: {only_integer: true, greater_than: 0, less_than: 100, allow_blank: true}
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+    has_one :cartitem, dependent: :destroy
+    has_many :cartitem_carts, through: :cartitems, source: :cart
     validates :name, presence: true, uniqueness: true
     validates :price, presence: true
 end
